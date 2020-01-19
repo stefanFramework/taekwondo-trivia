9e40578ba60498f808a8b52470b40a6ee3d62689
stefanFramework <cesar_cappetto@yahoo.com.ar>
2020-01-19 20:22:54 -0300

    First migrations + Migrations setup

--- src/database/migrations/2020_01_19_231055_create_users_table.php
+++ src/database/migrations/2020_01_19_231055_create_users_table.php
@@ -14,13 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->increments('id');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('email');
             $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->unique('email');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
